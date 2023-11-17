07e15beede079d8336dd80915329c5cbcaa32e35
C Karthikesavan <52634703+cskk7@users.noreply.github.com>
2023-11-17 13:20:44 +0530

    Create AnotherPackage.java

---
+++ AnotherPackage.java
@@ -0,0 +1,21 @@
+package Package2;
+
+public class AnotherPackage {
+    public void publicMethod(){
+        System.out.println("This is public method.");
+    }
+
+
+    private void privateMethod(){
+        System.out.println("This is private method");
+    }
+
+    protected void proctectedMethod() {
+        System.out.println("This is protected method");
+    }
+
+    void defaultMethod() {
+        System.out.println("This is private method");
+    }
+
+}
